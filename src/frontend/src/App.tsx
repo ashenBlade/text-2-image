932b72181c93e03eca46d633aed711387922a853
ash.blade <ashen.blade.dev@gmail.com>
2022-10-06 11:11:35 +0300

    add Ant Design package

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
       <div>
-        Hello, Text-2-Image
+        Hello, Text-2-Image with Ant Design
       </div>
   );
 }
