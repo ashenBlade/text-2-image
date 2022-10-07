71654b63d592c4cfec5d51c9b3e42945995d01a9
ash.blade <ashen.blade.dev@gmail.com>
2022-10-07 19:36:54 +0300

    refactor browser native handlers

--- src/web/routers/image_to_text.py
+++ src/web/routers/image_to_text.py
@@ -1,4 +1,5 @@
 import logging
+from http import HTTPStatus
 from io import BytesIO
 
 from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
@@ -16,12 +17,6 @@ def is_image_supported(image_extension: str):
     return image_extension.lower() in SUPPORTED_IMAGE_EXTENSIONS
 
 
-def create_headers_for_file_response(filename: str):
-    return {
-        'Content-Disposition': f'attachment; filename="{filename}"'
-    }
-
-
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
@@ -29,31 +24,25 @@ logger = logging.getLogger(__name__)
 @router.post('/api/image/to/text', response_class=PlainTextResponse)
 async def post__image_to_text(file: UploadFile = File(),
                               image_loader: TextImageLoader = Depends(get_text_image_loader)):
-    async def extract_text_image():
-        if not file:
-            raise HTTPException(status_code=400, detail='File not provided')
+    if not file:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='Image file not provided')
 
-        file_bytes = await file.read()
-        if not file_bytes:
-            raise HTTPException(status_code=400, detail='Provided file is empty')
+    file_bytes = await file.read()
 
-        io = BytesIO(file_bytes)
-        return image_loader.load(io)
+    if not len(file_bytes):
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail='Provided image file is empty')
 
-    def format_response_filename():
-        split = file.filename.rstrip('.').split('.')
-        if len(split) > 2 and split[-2].lower() == 'txt':
-            return '.'.join(split[:-1:1])
-        return f'{file.filename}.txt'
+    io = BytesIO(file_bytes)
 
     try:
-        image = await extract_text_image()
+        image = image_loader.load(io)
         return PlainTextResponse(image.text,
                                  status_code=200,
-                                 media_type='text/plain',
-                                 headers=create_headers_for_file_response(format_response_filename()))
+                                 media_type='text/plain')
     except (ValueError, UnicodeDecodeError):
-        raise HTTPException(status_code=422, detail='Could not convert given image to text')
+        raise HTTPException(status_code=422,
+                            detail='Could not convert given image to text')
+
 
 
 
--- src/web/routers/text_to_image.py
+++ src/web/routers/text_to_image.py
@@ -2,7 +2,7 @@ import logging
 from http import HTTPStatus
 from io import BytesIO
 
-from fastapi import APIRouter, File, UploadFile, Form, HTTPException, Depends
+from fastapi import APIRouter, Form, HTTPException, Depends
 from starlette.responses import StreamingResponse
 
 from text_to_image import TextImage, TextImageSaver
@@ -17,7 +17,7 @@ SUPPORTED_IMAGE_EXTENSIONS = {
 }
 
 
-def is_image_supported(image_extension: str):
+def is_image_extension_supported(image_extension: str):
     return image_extension.lower() in SUPPORTED_IMAGE_EXTENSIONS
 
 
@@ -31,73 +31,25 @@ def write_image_to_bytes_io(text: str,
         return buffer
     except ValueError as value:
         text_to_image_logger.info('Could not convert text to image: "%s"', text, exc_info=value)
-        raise HTTPException(status_code=500, detail='Could not convert given text. Internal error. Sorry')
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR,
+                            detail='Could not convert given text. Internal error. Sorry')
 
 
-def create_headers_for_file_response(filename: str):
-    return {
-        'Content-Disposition': f'attachment; filename="{filename}"'
-    }
-
-
-def text_to_image_internal(text: str, filename: str, extension: str, image_saver: TextImageSaver):
-    if len(text) == 0:
-        raise HTTPException(status_code=400,
+@router.post('/api/text/to/image', response_class=StreamingResponse)
+async def post__text_to_image(text: str = Form(),
+                              image_extension: str = Form('png'),
+                              image_saver: TextImageSaver = Depends(get_text_image_saver)):
+    if not len(text):
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,
                             detail='Input text must not be empty')
 
-    if not is_image_supported(extension):
+    if not is_image_extension_supported(image_extension):
         raise HTTPException(status_code=HTTPStatus.UNSUPPORTED_MEDIA_TYPE,
-                            detail=f"Image extension '{extension}' is not supported")
+                            detail=f"Image extension '{image_extension}' is not supported")
 
     text_to_image_logger.info('Convert of string with length: %i', len(text))
     response = write_image_to_bytes_io(text, image_saver)
     return StreamingResponse(response,
-                             media_type=f'image/{extension}',
-                             status_code=200,
-                             headers=create_headers_for_file_response(filename))
-
-
-@router.post('/api/text/to/image', response_class=StreamingResponse)
-async def post__text_to_image(file: UploadFile = File(None),
-                              text: str = Form(None),
-                              image_extension: str = 'png',
-                              filename: str = None,
-                              image_saver: TextImageSaver = Depends(get_text_image_saver)):
-    default_encoding = 'utf-8'
-    default_filename = 'encrypted'
-
-    def get_filename():
-        filename_inner = filename or (file.filename if file else default_filename)
-        return f'{filename_inner}.{image_extension}'
-
-    async def get_file_content_text():
-        try:
-            file_contents = await file.read()
-            decoded = file_contents.decode(default_encoding)
-            return decoded
-        except UnicodeDecodeError:
-            raise HTTPException(status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-                                detail="Could not parse input file. "
-                                       f"File encoding must be '{default_encoding}'")
-
-    if text and file:
-        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,
-                            detail='Choose either file or plain text')
-
-    if file:
-        return text_to_image_internal(await get_file_content_text(),
-                                      get_filename(),
-                                      image_extension,
-                                      image_saver)
-
-    if text:
-        return text_to_image_internal(text,
-                                      get_filename(),
-                                      image_extension,
-                                      image_saver)
-
-    raise HTTPException(status_code=400,
-                        detail='Text or file not provided')
-
-
+                             media_type=f'image/{image_extension}',
+                             status_code=200)
 
