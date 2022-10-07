76b3e239b0fb3bcd9a10e11e96dacab8e681128d
ash.blade <ashen.blade.dev@gmail.com>
2022-10-07 20:09:35 +0300

    add cors and remove static files

--- src/web/main.py
+++ src/web/main.py
@@ -1,8 +1,7 @@
 import logging
-import os
 
 from fastapi import FastAPI
-from starlette.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import (
     image_to_text_router,
@@ -12,13 +11,30 @@ from .routers import (
 logging.basicConfig(format="%(asctime)s - [%(levelname)s] -  %(name)s - %(filename)s.%(funcName)s - %(message)s")
 
 
-def create_app():
-    fast_api = FastAPI()
+def setup_middleware(fast_api: FastAPI):
+    if not fast_api.debug:
+        raise RuntimeError('Need to configure CORS for production')
+
+    fast_api.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+
+
+def setup_routers(fast_api):
     fast_api.include_router(image_to_text_router)
     fast_api.include_router(text_to_image_router)
-    fast_api.mount('/',
-                   StaticFiles(directory=os.path.join(os.path.dirname(__file__), 'static'),
-                               html=True))
+
+
+def create_app():
+    fast_api = FastAPI()
+
+    setup_middleware(fast_api)
+    setup_routers(fast_api)
+
     return fast_api
 
 
